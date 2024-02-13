07218bbeb489c0bd1690d085c7eb2b22273403b8
SyedAliAhmedShah <81427563+SyedAliAhmedShah@users.noreply.github.com>
2024-02-13 15:28:59 +0000

    Added about

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template('home.html')
 
+@app.route("/about")
+def hello_about():
+    return render_template('about.html')
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
